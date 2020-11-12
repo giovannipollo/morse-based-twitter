f656996873f243404df9c977432a3367c442b890
Giovanni Pollo <giovannipollo98@gmail.com>
2020-11-12 12:38:53 +0100

    Add extended task and memory optimization

--- assignment.cpp
+++ assignment.cpp
@@ -3,28 +3,7 @@
 #include "Arduino.h"
 #include "tpl_os.h"
 
-static int SOS[] = {
-    1,
-    0,
-    1,
-    1,
-    1,
-    1,
-    0,
-    1,
-    1,
-    1,
-    1,
-    0,
-    1,
-    1,
-    1,
-    1,
-    0,
-    1,
-    1,
-    1,
-};
+static const char SOS[] = {"10111101111011110111"};
 static int enable = 1;
 void setup() {
     Serial.begin(115200);
@@ -32,49 +11,86 @@ void setup() {
     pinMode(13, OUTPUT);
 }
 
-//The TASK is activated by the alarm "periodicAl":
-//* The alarm "periodicAl" is configured in the .oil file to activate task
-//"periodicTask" each 1000 pulses of counter SystemCounter.
-//* The SystemCounter is the same as the systick used in Arduino, a tick each 1024us on a 16MHz arduino.
-//* This task is periodic, with a period of 1024ms.
+/*
 TASK(periodicTask) {
+
     static int counter = 0;
-    static int counter_small = 0;
+    static int pause = 0;
+
     if (SOS[counter] && enable) {
         digitalWrite(13, HIGH);
-        Serial.println("1");
+        Serial.print("1");
+        Serial.print("       counter: ");
+        Serial.println(counter);
+
     } else {
         digitalWrite(13, LOW);
-        Serial.println("0");
+        Serial.print("0");
+        Serial.print("       counter: ");
+        if (enable) {
+            Serial.println(counter);
+        } else {
+            Serial.print(counter);
+            Serial.print("       pause: ");
+            Serial.println(pause - 1);
+        }
     }
     if (counter == 19) {
-
-        // 0.5s pause
-        if (counter_small == 5) {
-            counter_small = 0;
+        enable = 0;
+        if (pause == 5) {
+            pause = 0;
             counter = 0;
             enable = 1;
         } else {
-            enable = 0;
-            counter_small++;
+            pause++;
         }
     } else {
         counter++;
     }
+    TerminateTask();
 }
+*/
 
-/* 
-TASK(periodicTask2) {
-    EventMaskType prova;
+TASK(eventTask) {
+
+    static int counter = 0;
+    static int pause = 0;
+    EventMaskType one_sec_mask;
+
     while (1) {
-        WaitEvent(Eventprova);
-        GetEvent(periodicTask2, &prova);
-        if (prova) {
-            ClearEvent(Eventprova);
-            enable = !enable;
-            Serial.print("enable: ");
-            Serial.println(enable);
+        WaitEvent(one_sec);
+        GetEvent(eventTask, &one_sec_mask);
+        if (one_sec_mask) {
+            ClearEvent(one_sec);
+            if (SOS[counter] && enable) {
+                digitalWrite(13, HIGH);
+                Serial.print("1");
+                Serial.print("       counter: ");
+                Serial.println(counter);
+            } else {
+                digitalWrite(13, LOW);
+                Serial.print("0");
+                Serial.print("       counter: ");
+                if (enable) {
+                    Serial.println(counter);
+                } else {
+                    Serial.print(counter);
+                    Serial.print("       pause: ");
+                    Serial.println(pause - 1);
+                }
+            }
+            if (counter == 19) {
+                enable = 0;
+                if (pause == 5) {
+                    pause = 0;
+                    counter = 0;
+                    enable = 1;
+                } else {
+                    pause++;
+                }
+            } else {
+                counter++;
+            }
         }
     }
 }
-*/
