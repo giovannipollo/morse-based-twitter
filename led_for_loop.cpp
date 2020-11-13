fd7dacdc6e8646fa01535c9e02278cee3770ae8b
Giovanni Pollo <giovannipollo98@gmail.com>
2020-11-13 10:52:38 +0100

    Added 180s & single for loop for the led ON or OFF

--- led_for_loop.cpp
+++ led_for_loop.cpp
@@ -3,7 +3,10 @@
 #include "Arduino.h"
 #include "tpl_os.h"
 
-char prova[] = "CIAO";
+static const char *frasi[] = {
+    "CIAO",
+    "AOIC",
+};
 
 void setup() {
     Serial.begin(115200);
@@ -13,64 +16,76 @@ void setup() {
 
 TASK(periodicTask) {
 
+    char led = '0';
+    int index = 0;
+    int cnt = 0;
     EventMaskType mask;
     while (1) {
-        for (int i = 0; i < 4; i++) {
-            if (prova[i] == 'C') {
-                for (int j = 0; j < 4; j++) {
+        for (int k = 0; k < 2; k++) {
+            for (int i = 0; i < 4; i++) {
+                if (frasi[k][i] == 'C') {
+                    index = 4;
+                    led = '0';
+                }
+                if (frasi[k][i] == 'I') {
+                    index = 2;
+                    led = '1';
+                }
+                if (frasi[k][i] == 'A') {
+                    index = 1;
+                    led = '0';
+                }
+                if (frasi[k][i] == 'O') {
+                    index = 4;
+                    led = '1';
+                }
+
+                // write value of leds
+
+                for (int i = 0; i < index && cnt < 180; i++) {
                     WaitEvent(evento);
                     GetEvent(periodicTask, &mask);
                     if (mask) {
                         ClearEvent(evento);
-                        digitalWrite(13, LOW);
-                        Serial.println("0");
+                        if (led == '1') {
+                            digitalWrite(13, HIGH);
+                            Serial.print("1");
+                            Serial.print("         cnt = ");
+                            Serial.println(cnt);
+                        } else {
+                            digitalWrite(13, LOW);
+                            Serial.print("0");
+                            Serial.print("         cnt = ");
+                            Serial.println(cnt);
+                        }
+                        cnt++;
+                    }
+                }
+
+                // check if the end of the loop restart
+
+                if (i == 3) {
+                    i = -1; // -1 Because at the next cycle it will increment by 1 and so it will return to 0
+                }
+
+                // pause
+
+                if (cnt == 180) {
+                    i = 3; // così da uscire dal loop
+                    cnt = 0;
+                    for (int j = 0; j < 5; j++) {
+                        WaitEvent(evento);
+                        GetEvent(periodicTask, &mask);
+                        if (mask) {
+                            ClearEvent(evento);
+                            digitalWrite(13, LOW);
+                            Serial.println("0         Pause");
+                        }
                     }
                 }
             }
-            if (prova[i] == 'I') {
-                for (int j = 0; j < 2; j++) {
-                    WaitEvent(evento);
-                    GetEvent(periodicTask, &mask);
-                    if (mask) {
-                        ClearEvent(evento);
-                        digitalWrite(13, HIGH);
-                        Serial.println("1");
-                    }
-                }
-            }
-            if (prova[i] == 'A') {
-                for (int j = 0; j < 1; j++) {
-                    WaitEvent(evento);
-                    GetEvent(periodicTask, &mask);
-                    if (mask) {
-                        ClearEvent(evento);
-                        digitalWrite(13, LOW);
-                        Serial.println("0");
-                    }
-                }
-            }
-            if (prova[i] == 'O') {
-                for (int j = 0; j < 4; j++) {
-                    WaitEvent(evento);
-                    GetEvent(periodicTask, &mask);
-                    if (mask) {
-                        ClearEvent(evento);
-                        digitalWrite(13, HIGH);
-                        Serial.println("1");
-                    }
-                }
-            }
-            if (i == 3) {
-                i = -1;
-                for (int j = 0; j < 5; j++) {
-                    WaitEvent(evento);
-                    GetEvent(periodicTask, &mask);
-                    if (mask) {
-                        ClearEvent(evento);
-                        digitalWrite(13, LOW);
-                        Serial.println("0         Pause");
-                    }
-                }
+            if (k == 1) {
+                k = -1; // -1 Because at the next cycle it will increment by 1 and so it will return to 0
             }
         }
     }
